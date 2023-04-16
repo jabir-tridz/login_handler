3326f90197bc71983a85de4115086daefdbd3c99
jabir-tridz <jabir@tridz.com>
2023-04-16 10:08:31 +0000

    second commit

--- apcc/login_redirect.py
+++ apcc/login_redirect.py
@@ -3,7 +3,7 @@ import frappe
 @frappe.whitelist()
 def activity_log(user):
     doctype_data = frappe.db.get_all("Activity Log",fields = ['name'], filters= {'user': user, 'operation': 'Login','status': 'Success'})
-    if len(doctype_data == 1):
+    if len(doctype_data) == 1:
         return True
     else:
         return False
