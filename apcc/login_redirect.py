aa82ffc9aa416280335ec5d89fa7c7b41f4f9f70
jabir-tridz <jabir@tridz.com>
2023-04-18 09:19:21 +0000

    role based redirection

--- apcc/login_redirect.py
+++ apcc/login_redirect.py
@@ -2,8 +2,16 @@ import frappe
 
 @frappe.whitelist()
 def activity_log(user):
-    doctype_data = frappe.db.get_all("Activity Log",fields = ['name'], filters= {'user': user, 'operation': 'Login','status': 'Success'})
-    if len(doctype_data) == 1:
-        return True
-    else:
-        return False
+    redirect_status = False
+    user_data = frappe.get_doc("User",user)
+    roles = user_data.roles
+    for role in roles:
+        if role.role == 'System Manager':
+            activity_logs = frappe.db.get_all("Activity Log",fields = ['name'], filters= {'user': user, 'operation': 'Login','status': 'Success'})
+            if len(activity_logs) == 1:
+                redirect_status = True
+            else:
+                redirect_status = False
+        if role.role == 'Agent':
+            redirect_status = True
+    return redirect_status
